983bceb7f27f28f6037d5e7795f209345e89a5de
keyyeh <dangkiet200323@gmail.com>
2025-04-16 12:10:53 +0700

    update

--- backend/frontend/src/pages/OrderDetail/OrderDetailPage.jsx
+++ backend/frontend/src/pages/OrderDetail/OrderDetailPage.jsx
@@ -10,7 +10,10 @@ const OrderDetails = () => {
     const [orderDetails, setOrderDetails] = useState(null);
     const [message, setMessage] = useState(null);
     const [loading, setLoading] = useState(true);
-
+    //Trở về lại trang Oreder
+    const hanldReturnBackOrder = () => {
+        navigate('/order');
+    };
     const fetchOrderDetail = async () => {
         try {
             setLoading(true); // Bắt đầu gọi API
@@ -21,7 +24,15 @@ const OrderDetails = () => {
                 setMessage('Không tìm thấy chi tiết đơn hàng');
             }
         } catch (err) {
-            setMessage('Không lấy được chi tiết đơn hàng');
+            setMessage(
+                <div>
+                    Không tìm thấy chi tiết đơn hàng
+                    <br></br>
+                    <a className="back-btn" onClick={() => hanldReturnBackOrder()} type='button'>
+                        Quay lại danh sách
+                    </a>
+                </div>,
+            );
             console.error(err);
         } finally {
             setLoading(false); // Kết thúc gọi API
@@ -35,12 +46,15 @@ const OrderDetails = () => {
     // Xử lý trạng thái loading và lỗi
     if (loading) return <div>Đang tải...</div>;
     if (message) return <div>{message}</div>;
-    if (!orderDetails) return <div>Không có dữ liệu</div>;
-
-    //Trở về lại trang Oreder
-    const hanldReturnBackOrder = () => {
-        navigate('/order');
-    };
+    if (!orderDetails)
+        return (
+            <div>
+                Không có dữ liệu
+                <a className="back-btn" onClick={() => hanldReturnBackOrder()}>
+                    Quay lại danh sách
+                </a>
+            </div>
+        );
 
     //Lấy giá trị phần tử đầu tiên
     const detail = orderDetails[0];
@@ -48,32 +62,32 @@ const OrderDetails = () => {
     return (
         <div className="order-container">
             <h1>Chi tiết đơn đặt hàng</h1>
-            
-                <div className="order-details">
-                    <div className="detail-row">
-                        <span className="label">Mã đơn:</span>
-                        <span className="value">{detail.order?.id || 'N/A'}</span>
-                    </div>
-                    <div className="detail-row">
-                        <span className="label">Khách hàng:</span>
-                        <span className="value">{detail.order?.user?.email || 'N/A'}</span>
-                    </div>
-                    <div className="detail-row">
-                        <span className="label">Ngày đặt:</span>
-                        <span className="value">{detail.created_at || 'N/A'}</span>
-                    </div>
-                    <div className="detail-row">
-                        <span className="label">Số tiền:</span>
-                        <span className="value">{detail.order?.total_price || 'N/A'}</span>
-                    </div>
-                    <div className="detail-row">
-                        <span className="label">Trạng thái:</span>
-                        <span className={`value status ${detail.order?.status || ''}`}>
-                            {detail.order?.status || 'N/A'}
-                        </span>
-                    </div>
+
+            <div className="order-details">
+                <div className="detail-row">
+                    <span className="label">Mã đơn:</span>
+                    <span className="value">{detail.order?.id || 'N/A'}</span>
                 </div>
- 
+                <div className="detail-row">
+                    <span className="label">Khách hàng:</span>
+                    <span className="value">{detail.order?.user?.email || 'N/A'}</span>
+                </div>
+                <div className="detail-row">
+                    <span className="label">Ngày đặt:</span>
+                    <span className="value">{detail.created_at || 'N/A'}</span>
+                </div>
+                <div className="detail-row">
+                    <span className="label">Số tiền:</span>
+                    <span className="value">{detail.order?.total_price || 'N/A'}</span>
+                </div>
+                <div className="detail-row">
+                    <span className="label">Trạng thái:</span>
+                    <span className={`value status ${detail.order?.status || ''}`}>
+                        {detail.order?.status || 'N/A'}
+                    </span>
+                </div>
+            </div>
+
             <div className="ordered-products">
                 <h2>Danh sách sản phẩm</h2>
                 <table>
@@ -102,7 +116,7 @@ const OrderDetails = () => {
                 </table>
             </div>
 
-            <a className="back-btn" onClick={() => hanldReturnBackOrder()}>
+            <a className="back-btn" onClick={() => hanldReturnBackOrder()} type='button'>
                 Quay lại danh sách
             </a>
         </div>
