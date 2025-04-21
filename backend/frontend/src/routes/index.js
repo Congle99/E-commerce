d713acae9141975e16762e430edcb9952368eed7
DiemTrinh <tdtring@gmail.com>
2025-04-21 20:41:59 +0700

    Trang chủ và Trang Shop

--- backend/frontend/src/routes/index.js
+++ backend/frontend/src/routes/index.js
@@ -2,12 +2,25 @@ import Home from "~/pages/Admin/Home";
 import Product from "~/pages/Admin/Product/Product.jsx";
 import OrderPage from "~/pages/Admin/Order/OrderPage.jsx";
 import OrderDetailPage from "../pages/Admin/OrderDetail/OrderDetailPage.jsx";
+import Home from "../pages/User/Home/HomeUser.jsx"
+
+export const publicRoute = [
+    { path: '/', component: UserHome }, // Trang chủ người dùng
+];
+
+
+export const publicRoute = [
+    { path: '/', component: UserHome }, // Trang chủ người dùng
+];
+
 
 const publicRoute = [
     {path: '/', component: Home},
-    {path: '/product', component: Product},
-    {path: '/order', component: OrderPage},
-    {path: '/order/order-details/:id', component: OrderDetailPage},
+
+    // {path: '/', component: Home},
+    // {path: '/product', component: Product},
+    // {path: '/order', component: OrderPage},
+    // {path: '/order/order-details/:id', component: OrderDetailPage},
 ]
 const privateRoute = [
 
