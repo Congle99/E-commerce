260bc17ad19dde23894c8f4d45b03eed50926e65
phuoc2402 <sinolatui@gmail.com>
2025-05-17 08:59:38 +0700

    hoàn thành payment

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -11,13 +11,14 @@ import Shop from "../pages/User/Shop/Shop.jsx";
 import ProductDetail from "../pages/User/ProductDetail/ProductDetail.jsx";
 import Cart from "../pages/User/Cart/Cart.jsx";
 import PromotionPage from "../pages/Admin/Promotion/PromotionPage.jsx";
-
+import Payment from "../pages/User/Payment/Checkout.jsx";
 // Dành cho User
 const UserRoute = [
   { path: "/user", component: HomeUser },
   { path: "/ShopUser", component: Shop },
   { path: "/ProductUser/:id", component: ProductDetail },
   { path: "/CartUser", component: Cart },
+  { path: "/checkout", component: Payment },
 ];
 
 // Dành cho admin
